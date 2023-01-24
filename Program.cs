85bf08d50d4d55214cdec23384e0e5d5dae4585f
EqinHo <Quyen.Ho@gokstadakademiet.no>
2023-01-24 12:10:26 +0100

    Jeg begynner å lære git og github!

--- Program.cs
+++ Program.cs
@@ -2,7 +2,18 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        k 
+        //Console.WriteLine("Hello, World!");
+        MyClass myclass = new MyClass();
+ /*       if (myclass.IsInitialized)
+        {
+            Console.WriteLine("myclass is initalized");
+
+        }
+ */
+        Console.WriteLine(myclass);//
+        myclass.greetings();
+        bool test = true;
+      
     }
+    
 }
---
+++ src/MyClass.cs
@@ -0,0 +1,22 @@
+﻿internal class MyClass
+{
+    public bool IsInitialized;
+    public MyClass()
+    {
+        Console.WriteLine("Hello, World from Class MyClass!");
+        IsInitialized = true;
+    }
+    public override string ToString()
+    {
+        return "MyClass.IsInitialized : " + IsInitialized;
+    }
+    public void greetings() 
+    {
+        Console.WriteLine("What is your name?");
+        // ask for name
+        var name = Console.ReadLine();
+        // get date now
+        var currentDate = DateTime.Now;
+        Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+    }
+}
